9cf30e8c073c30e0a5f3302490afa85fab7158cf
Omukahmed <omukahmed600@gmail.com>
2024-06-25 16:48:32 +0600

    Create download.js

---
+++ js/download.js
@@ -0,0 +1,45 @@
+    // QuadDividerPro 
+    document.getElementById('downloadqd').addEventListener('click', function() {
+        var filePath1 = '/app/QuadDividerPro_v1.3.apk';
+        var anchor1 = document.createElement('a');
+        anchor1.href = filePath1;
+        anchor1.download = 'QuadDividerPro_v1.3.apk';
+        anchor1.click();
+    });
+    
+    // GnaCAD v
+    document.getElementById('downloadgnacad1').addEventListener('click', function() {
+        var filePath2 = '/app/QdpCAD_v7.apk';
+        var anchor2 = document.createElement('a');
+        anchor2.href = filePath2;
+        anchor2.download = 'QdpCAD_v7.apk';
+         anchor2.click();
+    });
+    
+    
+    // Bangla converter
+    document.getElementById('downloadgnacad2').addEventListener('click', function() {
+        var filePath4 = '/app/banglaconverter.apk';
+        var anchor4 = document.createElement('a');
+        anchor4.href = filePath4;
+        anchor4.download = 'banglaconverter.apk';
+        anchor4.click();
+    });
+
+// satellite map
+    document.getElementById('satellite').addEventListener('click', function() {
+        var filePath4 = '/app/satellite.apk';
+        var anchor4 = document.createElement('a');
+        anchor4.href = filePath4;
+        anchor4.download = 'satellite.apk';
+        anchor4.click();
+    });
+    
+//image meter 
+document.getElementById('image_meter').addEventListener('click', function() {
+        var filePath5 = '/app/image_meter.apk';
+        var anchor5 = document.createElement('a');
+        anchor5.href = filePath5;
+        anchor5.download = 'image_meter.apk';
+        anchor5.click();
+    });
